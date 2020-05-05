95062f63d6235dba614d6d6182f9c380be831c6c
Bexenious <xeniabelor@gmail.com>
2020-05-05 17:50:34 +0300

    add comment section
    added comment section and href links if needed

--- funct.js
+++ funct.js
@@ -4,3 +4,9 @@ $(function () {
         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
     });
 });
+
+n =  new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("day").innerHTML = m + "/" + d + "/" + y;
