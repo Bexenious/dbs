0e9d974a5a7a4585e575482c4b30e8c4862f7c1e
Bexenious <xeniabelor@gmail.com>
2020-05-06 20:06:51 +0300

    deleted website funct.js
    deleted website funct.js

--- funct.js
+++ funct.js
@@ -4,9 +4,3 @@ $(function () {
         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
     });
 });
-
-n =  new Date();
-y = n.getFullYear();
-m = n.getMonth() + 1;
-d = n.getDate();
-document.getElementById("day").innerHTML = m + "/" + d + "/" + y;
